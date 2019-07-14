68c39bcebed607535eaafd23b8095ee40eb2fd5b
Aakash Jadhav <aakashjadhav459@gmail.com>
2019-07-14 23:01:40 +0530

    React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,92 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React ,{Component} from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import {BrowserRouter as Router,Route} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
+
+
+import logo from "./logo.png";
+import CreateTodo from "./Component/create-todo.component";
+import EditTodo from "./Component/edit-todo.component";
+import TodosList from "./Component/todos-list.component";
+
+class App extends Component {
+
+  
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+  render(){
+    return (
+      <Router>
+        <div className="container">
+          <Navbar color="light" light expand="md">
+            <NavbarBrand href="http://www.google.co.in" target="_blank">
+              <img src={logo} width="60" height="60" alt="Todo App"></img>
+              Todo App</NavbarBrand>
+            <NavbarToggler onClick={this.toggle}/>
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <NavLink href="/">Todos</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/create">Create Todo</NavLink>
+                </NavItem>
+                <UncontrolledDropdown nav inNavbar>
+                  <DropdownToggle nav caret>
+                    Options
+                  </DropdownToggle>
+                  <DropdownMenu right>
+                    <DropdownItem>
+                      Option 1
+                    </DropdownItem>
+                    <DropdownItem>
+                      Option 2
+                    </DropdownItem>
+                    <DropdownItem divider />
+                    <DropdownItem>
+                      Reset
+                    </DropdownItem>
+                  </DropdownMenu>
+                </UncontrolledDropdown>
+              </Nav>
+            </Collapse>
+          </Navbar>
+
+
+          <Route path="/" exact component={TodosList}></Route>
+          <Route path="/edit/:id" component={EditTodo}></Route>
+          <Route path="/create" component={CreateTodo}></Route>  
+        </div>     
+      </Router>  
+    );  
+       
+  }
+  
 }
 
 export default App;
